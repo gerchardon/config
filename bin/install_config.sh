619d52576c1cea88d0e365f6c45cd6b8fc4eee21
Gérôme Chardon <gerome.chardon@gmail.com>
2015-10-11 16:46:09 +0200

    Refactor install script

--- bin/install_config.sh
+++ bin/install_config.sh
@@ -1,52 +1,44 @@
 #!/bin/sh
 
+##
+## Shell script to configure env
+##  * Install gerchardon/config
+##  * Run install/update scripts
+
 WHERE=${WHERE-$HOME/projects/config}
 
-packages="git curl terminator vim xclip wget"
-dpkg -s $packages >/dev/null 2>&1 && {
-  echo "Packages already install"
-} || {
-  sudo apt-get update
-  sudo apt-get install -y $packages
+
+## FUNCTION UTIL
+
+# Check or install packages $1
+function check_or_install_packages() {
+    packages=$@
+    dpkg -s $packages >/dev/null 2>&1 && {
+        echo "Packages already install"
+    } || {
+        sudo apt-get update
+        sudo apt-get install -y $packages
+    }
 }
 
-[ ! -d "$WHERE" ] && {
-  # Get repo
-  mkdir -p $WHERE/..
-  git clone --recursive https://github.com/gerchardon/config.git $WHERE
-} || {
-  # Refresh repo
-  git -C $WHERE pull
+function git_clone() {
+    repo=$1
+    dir=$2
+    [ ! -d "$repo"] && {
+        mkdir -p "$repo/.."
+        git clone --recursive $repo $dir
+    } || {
+        git -C $dir pull
+    }
 }
 
-# Configure Bash Aliases
-ln -sf $WHERE/.bash_aliases $HOME/
-ln -sf $WHERE/.alias.d/ $HOME/
-ln -sf $WHERE/bin $HOME/
+## START SCRIPT
 
-# Apps Config
-ln -sf $WHERE/terminator ~/.config/
+check_or_install_packages \
+    "git" "curl" \
+    "terminator" "vim" \
+    "xclip" "wget"
 
-# Git Config
-ln -sf $WHERE/git/.gitalias $HOME/
-ln -sf $WHERE/git/.giturl $HOME/
-git config --global --replace-all include.path "~/.gitalias" "~/.gitalias"
+git_clone "https://github.com/gerchardon/config.git" "$WHERE"
 
-# NodeJs Config (with nvm)
-if [ ! -d "$HOME/.nvm" ]; then
-  git clone github:creationix/nvm $HOME/.nvm
-else
-  # Get last tag
-  git -C "$HOME/.nvm" fetch --all >/dev/null 2>&1
-  git -C "$HOME/.nvm" checkout master >/dev/null 2>&1
-  git -C "$HOME/.nvm" pull >/dev/null 2>&1
-  git -C "$HOME/.nvm" checkout $(git -C "$HOME/.nvm" describe --abbrev=0 --tags) >/dev/null 2>&1
-fi
-. $HOME/.nvm/nvm.sh
-echo "Update iojs"
-nvm install iojs
-nvm alias default iojs
-
-# Python
-## virtualenvwrapper
-[ ! -f ~/.local/bin/virtualenvwrapper.sh ] && pip install --user --install-option="--user" virtualenvwrapper
+$WERE/bin/update_config
