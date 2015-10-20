486666458c13ae778559d798b6bdab8136520dc4
Gérôme Chardon <gerome.chardon@gmail.com>
2015-10-20 19:54:23 +0200

    Fix docker check

--- bin/check_docker.sh
+++ bin/check_docker.sh
@@ -2,4 +2,4 @@
 docker run --rm -it --net host --pid host --cap-add audit_control \
   -v /var/lib:/var/lib -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/lib/systemd:/usr/lib/systemd  -v /etc:/etc \
-  --label docker-bench-security diogomonica/docker-bench-security
+  --label docker_bench_security docker/docker-bench-security
