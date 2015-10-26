18c22ba209d7c801b28572bf2bbad77b0ee83f54
Gérôme Chardon <gerchardon@users.noreply.github.com>
2015-10-26 19:41:49 +0100

    Add git rewrite author sh

---
+++ bin/git-rewrite-author.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Usage:
+##  git clone --bare https://github.com/user/repo.git
+##  git-rewrite-author.sh <oldEmail> "<Name>" <newEmail>
+##  git push --force --tags origin 'refs/heads/*'
+
+[ ! $# -eq 3 ] && echo "Usage $(basename $0) <oldEmail> \"<Name>\" <email>" && exit 1
+
+RENAME_AUTHOR=$(cat <<EOF
+OLD_EMAIL="$1"
+CORRECT_NAME="$2"
+CORRECT_EMAIL="$3"
+if [ "\$GIT_COMMITTER_EMAIL" = "\$OLD_EMAIL" ]
+then
+  export GIT_COMMITTER_NAME="\$CORRECT_NAME"
+  export GIT_COMMITTER_EMAIL="\$CORRECT_EMAIL"
+fi
+if [ "\$GIT_AUTHOR_EMAIL" = "\$OLD_EMAIL" ]
+then
+  export GIT_AUTHOR_NAME="\$CORRECT_NAME"
+  export GIT_AUTHOR_EMAIL="\$CORRECT_EMAIL"
+fi
+EOF
+)
+
+git filter-branch --env-filter "$RENAME_AUTHOR" --tag-name-filter cat -- --branches --tags
