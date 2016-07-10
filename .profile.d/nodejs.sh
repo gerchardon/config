49a943c85c64d2933118fd283b89aba00c9b0cdf
Gérôme Chardon <gerchardon@users.noreply.github.com>
2016-07-10 22:03:50 +0200

    move bashrc to profile

---
+++ .profile.d/nodejs.sh
@@ -0,0 +1,5 @@
+# Nvm load
+export NODE_PATH=$NODE_PATH:~/.node/lib/node_modules
+export PATH=$PATH:~/.node/bin/
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
